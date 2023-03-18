1e517a9f4d7948c621c230b3c5bdebb8060a3751
nardo88 <nardo1988@mail.ru>
2023-03-18 11:06:39 +0300

    настроил получение данных

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,4 +1,14 @@
-import { FC, useCallback, useEffect, useState } from 'react'
+import {
+  FC,
+  MutableRefObject,
+  UIEvent,
+  useCallback,
+  useEffect,
+  useRef,
+  useState,
+} from 'react'
+import { useInfiniteScroll } from '../../hooks/useInfiniteScroll'
+import { useThrottle } from '../../hooks/useThrottle'
 import { ListItem } from '../ListItem/ListItem'
 import cls from './List.module.scss'
 
@@ -15,20 +25,45 @@ export interface IDataItem {
 
 export const List: FC<ListProps> = ({ className }) => {
   const [dataList, setDataList] = useState<IDataItem[]>([])
-  console.log('dataList: ', dataList)
   const [currentPage, setCurrentPage] = useState(1)
+  const [isLoading, setIsLoading] = useState(false)
+  console.log('currentPage: ', currentPage)
+  const TOTAL = 100
+
+  const wrapperRef = useRef() as MutableRefObject<HTMLDivElement>
+  const triggerRef = useRef() as MutableRefObject<HTMLDivElement>
+
+  useInfiniteScroll({
+    triggerRef,
+    wrapperRef,
+    callback: () => {
+      if (dataList.length < TOTAL && !isLoading) {
+        console.log('trigger')
+        setCurrentPage(currentPage + 1)
+        setIsLoading(true)
+      }
+    },
+  })
 
   const getData = useCallback(async () => {
-    const data: IDataItem[] = await fetch(
+    setIsLoading(true)
+
+    fetch(
       // eslint-disable-next-line
       `https://jsonplaceholder.typicode.com/photos?_limit=10&_page=${currentPage}`
-    ).then((res) => res.json())
-
-    setDataList((prev) => {
-      const newState = [...prev]
-      newState.push(...data)
-      return newState
-    })
+    )
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data)
+        setDataList((prev) => {
+          const newState = [...prev]
+          newState.push(...data)
+          return newState
+        })
+      })
+      .then(() => {
+        setIsLoading(false)
+      })
   }, [currentPage])
 
   useEffect(() => {
@@ -36,10 +71,11 @@ export const List: FC<ListProps> = ({ className }) => {
   }, [getData, currentPage])
 
   return (
-    <div className={cls.List}>
-      {dataList.map((item) => (
-        <ListItem key={item.id} {...item} />
+    <div className={cls.List} ref={wrapperRef}>
+      {dataList.map((item, i) => (
+        <ListItem key={item.id} {...item} index={i + 1} />
       ))}
+      <div ref={triggerRef} className={cls.trigger} />
     </div>
   )
 }
--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -2,13 +2,18 @@ import { FC } from 'react'
 import { IDataItem } from '../List/List'
 import cls from './ListItem.module.scss'
 
-interface ListItemProps extends IDataItem {}
+interface ListItemProps extends IDataItem {
+  index: number
+}
 
 export const ListItem: FC<ListItemProps> = (props) => {
-  const { albumId, id, title, url } = props
+  const { albumId, id, title, url, index } = props
   return (
     <div className={cls.ListItem}>
-      <p>{title}</p>
+      <p>
+        <span className={cls.number}>{index}</span>
+        {title}
+      </p>
       <img src={url} alt="" />
     </div>
   )
